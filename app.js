77705daa25222c1b339488ae4a2f68a50bf17624
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2022-10-14 12:13:55 -0600

    add about and contact pages

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(request, response) {
   response.render("home");
@@ -54,8 +60,47 @@ app.get("/pavlova", function(request, response) {
   response.render("pavlova");
 });
 
+app.get("/roasted-potatoes", function(request, response) {
+  response.render("roasted_potatoes");
+});
+
 app.get("/all", function(request, response) {
   response.render("all");
+});
+
+app.get("/about", function(request, response) {
+  response.render("about");
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact");
+});
+
+app.post("/contact", function(req, res) {
+  console.log(req.body);
+  var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASS
+      }
+    });
+    
+    var mailOptions = {
+      from: 'automatedsender12.90@gmail.com',
+      to: 'chris.oreilly97@gmail.com',
+      subject: 'New Message from COCO\'s',
+      text: "New message from "+ req.body.name + " at " + req.body.email + "\r\n" + req.body.name + "'s message:\r\n\r\n" + req.body.message
+  };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.render("contact-success");
+      }
+    });
 });
 
 let port = process.env.PORT;
